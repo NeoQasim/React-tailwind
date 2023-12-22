701c3fdf3b1772d02ff46186e3a3fa98d4ad7705
neoqasim <neoqasim@gmail.com>
2023-12-22 23:59:46 +0500

    created  the hero section

---
+++ src/components/Hero.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Typed from 'react-typed'
+
+import Navs from './Navs'
+
+const Hero = () => {
+    return (
+        <>
+            <Navs />
+            <div className='text-white'>
+                <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col gap-3 justify-center">
+                    <p className='text-[#ffdf41] text-2xl '>Lorem ipsum dolor sit amet.</p>
+                    <h1 className='font-bold text-3xl sm:text-4xl md:text-5xl lg:text-6xl sm:py-4'>lop contet adiing elit.</h1>
+                    <div className="bg-none flex text-2xl sm:text-3xl md:text-5xl lg:text-5xl justify-center nd gap-6">
+                        <p>Lorem ipsum dolor sitsits
+                        </p>
+                        <Typed className='shadow-lg shadow-yellow-400' strings={["Neo", "Qasim", "Khan"]} typeSpeed={60} backSpeed={50} loop />
+                    </div>
+                    <p className='text-slate-500 text-xl font-semibold'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, accusantium. Ullam dolore nesciunt quasi.!</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Hero
